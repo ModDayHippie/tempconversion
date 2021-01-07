263a2716a24aa19ab3589da73b78e77dce0151fe
ModDayHippie <48167631+ModDayHippie@users.noreply.github.com>
2021-01-07 11:31:23 -0500

    Initial commit

---
+++ tempconversion.py
@@ -0,0 +1,15 @@
+temp = input('what is the temperature:')
+
+ttype = input('f or c:')
+if ttype == 'f':
+    ftemp = int(temp) - 32
+    stemp = int(ftemp) * 5
+    ttemp = int(stemp) / 9
+    print(ttemp)
+if ttype == 'c':
+    cftemp = int(temp) / 5
+    cstemp = int(cftemp) * 9
+    cttemp = int(cstemp) + 32
+    print(cttemp)
+
+    input('press Enter to exit')
